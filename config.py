74021eaf0bc5920e5b8d14a8537912961ca936bd
Kovalev906090 <156798592+Kovalev906090@users.noreply.github.com>
2025-01-03 23:51:05 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "7450368478:AAFRivU2zwrXDf6xn2p-942ZGT4L9PcV0P8"
+CHANNEL_URL = "https://t.me/krfn3olifjnof"
+CHANNEL_ID = "-1002366905447"
+ADMIN_ID = 7053982067
+REF_URL = "https://1wqydy.top/casino/list?open=register&p=6vsi"
+PROMO_CODE = "ZASXEMY2024"
+BOT_NAME = "Mines Chat GPT 5.0"
